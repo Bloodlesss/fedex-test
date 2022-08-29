b9e803fc31355b2b48ecd27df33af5d6bb361380
Jamal Saleh <jamal.ngu@gmail.com>
2022-08-29 20:07:50 +0300

    adding test cases for user reception and singup

--- FedExSignUp/src/app/modules/auth/components/user-reception/user-reception.component.spec.ts
+++ FedExSignUp/src/app/modules/auth/components/user-reception/user-reception.component.spec.ts
@@ -1,23 +1,31 @@
+import { DebugElement, ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { SignedUpUser } from 'src/app/shared/models/signed-up-user';
+import { mockuserData } from 'src/specs/mock-data';
 
 import { UserReceptionComponent } from './user-reception.component';
 
 describe('UserReceptionComponent', () => {
   let component: UserReceptionComponent;
   let fixture: ComponentFixture<UserReceptionComponent>;
-
+  const mockedUserInfo: SignedUpUser = mockuserData;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserReceptionComponent ]
-    })
-    .compileComponents();
+      declarations: [UserReceptionComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserReceptionComponent);
     component = fixture.componentInstance;
+    component.userInfo = mockedUserInfo;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('changeState', () => {
+    it('should emit when onClick() is called', () => {
+      spyOn(component.changeState, 'emit');
+      component.goBack(); // call the onClick method directly
+      expect(component.changeState.emit).toHaveBeenCalledWith();
+    });
   });
 });
--- FedExSignUp/src/app/modules/auth/containers/sign-up/sign-up.component.spec.ts
+++ FedExSignUp/src/app/modules/auth/containers/sign-up/sign-up.component.spec.ts
@@ -1,19 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignUpState } from 'src/app/shared/enums/sign-up-state';
 import { SignedUpUser } from 'src/app/shared/models/signed-up-user';
+import { mockuserData } from 'src/specs/mock-data';
 
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
-  let mockedUserInfo: SignedUpUser  ={
-    _id: '',
-    email: '',
-    firstName: '',
-    lastName: '',
-  };
-  let mockedState:string=SignUpState.signUp;
+  let mockedUserInfo: SignedUpUser = mockuserData;
+  let mockedState: string = SignUpState.signUp;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SignUpComponent],
@@ -21,14 +17,22 @@ describe('SignUpComponent', () => {
 
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
-    component.state=mockedState;
-    component.userInfo=mockedUserInfo;
+    component.state = mockedState;
+    component.userInfo = mockedUserInfo;
     fixture.detectChanges();
   });
 
   describe('onSubmit', () => {
     it('should update userInfo and state', () => {
-      expect(component.userInfo).toHaveBeenCalled();
+      component.onSubmit(mockedUserInfo);
+      expect(component.userInfo).toEqual(mockedUserInfo);
+      expect(component.state).toEqual(SignUpState.showUser);
+    });
+  });
+  describe('changeStateToSignUp', () => {
+    it('should update state to SignUp', () => {
+      component.changeStateToSignUp();
+      expect(component.state).toEqual(SignUpState.signUp);
     });
   });
 });
